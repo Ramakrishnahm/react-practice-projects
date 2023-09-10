0327b004af1ed454515b6c180c7c9aa75d19e0dd
ramakrishnahm <ramakrishna729m@gmail.com>
2023-09-10 10:30:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import people from "./data";
+import "./App.css";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
-function App() {
+const App = () => {
+  const [position, setPosition] = useState(0);
+  const { name, job, image, text } = people[position];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article className="container">
+      <h1 style={{ color: "green" }}>Fectch Data from data.js</h1>
+      <h2>{name}</h2>
+      <h2>{job}</h2>
+      <img src={image} className="img" />
+      <p>{text}</p>
+      <FaChevronLeft />
+      <FaChevronRight />
+      <FaQuoteRight />
+    </article>
   );
-}
+};
 
 export default App;
